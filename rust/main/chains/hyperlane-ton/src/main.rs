abdc993d1698f1fff6acb3f3597336f6bba261d3
Lapo4kaKek <Sheydos2003@yandex.ru>
2024-10-03 08:41:37 +0000

    Initial implementation of HyperlaneProvider methods (get_block_by_hash, get_txn_by_hash, is_contract, get_balance, get_chain_metrics)

--- rust/main/chains/hyperlane-ton/src/main.rs
+++ rust/main/chains/hyperlane-ton/src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::anyhow;
 
+mod types;
+
 mod providers;
 use crate::providers::provider::TonProvider;
 
--- rust/main/chains/hyperlane-ton/src/providers/provider.rs
+++ rust/main/chains/hyperlane-ton/src/providers/provider.rs
@@ -12,10 +12,14 @@ use hyperlane_core::{
 use reqwest::{Client, Response};
 use tokio::{sync::RwLock, time::Sleep};
 
+use crate::types::transaction::TransactionResponse;
 use tonlib::{
     address::{TonAddress, TonAddressParseError},
     cell::{BagOfCells, Cell, CellBuilder, CellSlice},
-    client::{TonClient, TonClientBuilder, TonClientInterface, TonConnection, TonConnectionParams},
+    client::{
+        TonClient, TonClientBuilder, TonClientError, TonClientInterface, TonConnection,
+        TonConnectionParams,
+    },
     config::{MAINNET_CONFIG, TESTNET_CONFIG},
     contract::{JettonMasterContract, TonContractError, TonContractFactory, TonContractInterface},
     message::TransferMessage,
@@ -31,8 +35,8 @@ use tonlib::{
 
 #[derive(Clone, new)]
 pub struct TonProvider {
-    pub ton_client: Arc<RwLock<TonClient>>,
-    pub client: Arc<RwLock<Client>>,
+    pub ton_client: TonClient,
+    pub client: Client,
     pub domain: HyperlaneDomain,
 }
 impl std::fmt::Debug for TonProvider {
@@ -56,46 +60,141 @@ impl HyperlaneChain for TonProvider {
 
 #[async_trait]
 impl HyperlaneProvider for TonProvider {
-    #[doc = " Get block info for a given block hash"]
-    #[must_use]
-    #[allow(
-        elided_named_lifetimes,
-        clippy::type_complexity,
-        clippy::type_repetition_in_bounds
-    )]
     async fn get_block_by_hash(&self, hash: &H256) -> ChainResult<BlockInfo> {
-        todo!()
+        // need check
+        let block_id: BlockIdExt = BlockIdExt {
+            workchain: 0,
+            shard: 0,
+            seqno: 0,
+            root_hash: hash.to_string(),
+            file_hash: "".to_string(),
+        };
+        let client = self.ton_client.get_block_header(&block_id).await;
+
+        match client {
+            Ok(blocks_header) => {
+                let block_info: BlockInfo = BlockInfo {
+                    hash: H256::from_slice(&blocks_header.id.root_hash.as_bytes()),
+                    timestamp: blocks_header.gen_utime as u64,
+                    number: blocks_header.id.seqno as u64,
+                };
+                Ok(block_info)
+            }
+            Err(e) => Err(ChainCommunicationError::Other(
+                HyperlaneCustomErrorWrapper::new(Box::new(e)),
+            )),
+        }
     }
 
-    #[must_use]
-    #[allow(
-        elided_named_lifetimes,
-        clippy::type_complexity,
-        clippy::type_repetition_in_bounds
-    )]
     async fn get_txn_by_hash(&self, hash: &H256) -> ChainResult<TxnInfo> {
-        todo!()
+        let url = "https://toncenter.com/api/v3/transactions";
+
+        // let mut request = self.client.read().await.get(url).query(&[("hash", hash)]);
+
+        let response = self
+            .client
+            .await
+            .get(url)
+            .query(&[("hash", format!("{:?}", hash))])
+            .send()
+            .await
+            .map_err(|e| {
+                warn!("Error when sending request to TON API");
+                ChainCommunicationError::Other(HyperlaneCustomErrorWrapper::new(Box::new(e)))
+            })?
+            .json::<TransactionResponse>()
+            .await
+            .map_err(|e| {
+                warn!("Error deserializing response from TON API");
+                ChainCommunicationError::Other(HyperlaneCustomErrorWrapper::new(Box::new(e)))
+            })?;
+
+        if let Some(transaction) = response.transactions.first() {
+            let txn_info = TxnInfo {
+                hash: H256::from_slice(&hex::decode(&transaction.hash).unwrap()),
+                gas_limit: U256::from_dec_str(&transaction.description.compute_ph.gas_limit)
+                    .unwrap_or_default(),
+                max_priority_fee_per_gas: None,
+                max_fee_per_gas: None,
+                gas_price: None,
+                nonce: transaction.lt.parse::<u64>().unwrap_or(0),
+                sender: H256::from_slice(&hex::decode(&transaction.account).unwrap()),
+                recipient: transaction
+                    .in_msg
+                    .as_ref()
+                    .map(|msg| H256::from_slice(&hex::decode(&msg.destination).unwrap())),
+                receipt: Some(TxnReceiptInfo {
+                    gas_used: U256::from_dec_str(&transaction.description.compute_ph.gas_used)
+                        .unwrap_or_default(),
+                    cumulative_gas_used: U256::zero(),
+                    effective_gas_price: None,
+                }),
+            };
+            info!("Successfully retrieved transaction: {:?}", txn_info);
+
+            Ok(txn_info)
+        } else {
+            warn!("No transaction found for the provided hash");
+            Err(ChainCommunicationError::Other(
+                HyperlaneCustomErrorWrapper::new(Box::new("No transactions found")),
+            ))
+        }
     }
 
-    #[allow(
-        elided_named_lifetimes,
-        clippy::type_complexity,
-        clippy::type_repetition_in_bounds
-    )]
     async fn is_contract(&self, address: &H256) -> ChainResult<bool> {
-        todo!()
+        info!("Checking if contract exists at address: {:?}", address);
+
+        let ton_address = TonAddress::from_base64_url(&address.to_string()).map_err(|e| {
+            ChainCommunicationError::Other(HyperlaneCustomErrorWrapper::new(Box::new(e)))
+        })?;
+
+        let account_state = self
+            .ton_client
+            .get_account_state(&ton_address)
+            .await
+            .map_err(|e| {
+                warn!("Failed to get account state: {:?}", e);
+                ChainCommunicationError::Other(HyperlaneCustomErrorWrapper::new(Box::new(e)))
+            })?;
+
+        match account_state.account_state {
+            AccountState::Uninited { .. } => {
+                info!("Account is uninitialized.");
+                Ok(false)
+            }
+            _ => {
+                info!("Account is initialized.");
+                Ok(true)
+            }
+        }
     }
 
     async fn get_balance(&self, address: String) -> ChainResult<U256> {
-        todo!()
+        info!("Try send get_balance request for: {:?}", address);
+
+        let address = TonAddress::from_base64_url(address.as_str());
+        match address {
+            Ok(address) => {
+                let read_guard = self.ton_client.await;
+                let account_state = read_guard.get_account_state(&address).await.map_err(|e| {
+                    info!("Error while getting account state: {:?}", e);
+                    ChainCommunicationError::Other(HyperlaneCustomErrorWrapper::new(Box::new(e)))
+                })?;
+
+                info!("Get account state success, data: {:?}", account_state);
+
+                let balance: i64 = account_state.balance;
+                let balance_u256: U256 = U256::from(balance);
+
+                Ok(balance_u256)
+            }
+            Err(error) => Err(ChainCommunicationError::Other(
+                HyperlaneCustomErrorWrapper::new(Box::new(error)),
+            )),
+        }
     }
 
-    #[allow(
-        elided_named_lifetimes,
-        clippy::type_complexity,
-        clippy::type_repetition_in_bounds
-    )]
     async fn get_chain_metrics(&self) -> ChainResult<Option<ChainInfo>> {
-        todo!()
+        Ok(None)
     }
 }
