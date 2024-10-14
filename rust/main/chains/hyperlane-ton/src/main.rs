92d5a402cc0b90e767087144424c6729d1a28556
Lapo4kaKek <Sheydos2003@yandex.ru>
2024-10-14 12:27:05 +0000

    resolve lifetimes problems in TonMailbox implementation

--- rust/main/chains/hyperlane-ton/src/main.rs
+++ rust/main/chains/hyperlane-ton/src/main.rs
@@ -1,6 +1,6 @@
 use crate::trait_builder::TonConnectionConf;
 use crate::traits::ton_api_center::TonApiCenter;
-use hyperlane_core::{HyperlaneDomain, HyperlaneProvider, KnownHyperlaneDomain};
+use hyperlane_core::{HyperlaneDomain, HyperlaneProvider, KnownHyperlaneDomain, Mailbox, H256};
 use tonlib::cell::ArcCell;
 use tonlib::cell::CellSlice;
 use tonlib::client::TonClient;
@@ -20,6 +20,7 @@ use tonlib::{
 use url::Url;
 
 use crate::client::provider::TonProvider;
+use crate::contracts::mailbox::TonMailbox;
 use log::info;
 use pretty_env_logger::env_logger;
 use reqwest::Client;
@@ -37,6 +38,34 @@ async fn main() -> anyhow::Result<()> {
 
     let ton_client = create_mainnet_client().await;
     let http_client = Client::new();
+
+    let connection_config = TonConnectionConf::new(
+        Url::parse("https://testnet.toncenter.com/api/")?,
+        "d0e66cd5bae6419130bc8b3b7e9ee6c675678d21be5f30c1b30619b219d27505".to_string(),
+    );
+
+    let domain = HyperlaneDomain::Known(KnownHyperlaneDomain::Sepolia);
+
+    let mailbox = TonMailbox {
+        mailbox_address: TonAddress::from_base64_url(
+            "EQASffmsB4eQl0wJ4QlwD47fPtI68pbClgygrIe8H5y-SUjB",
+        )
+        .unwrap(),
+        provider: TonProvider {
+            ton_client,
+            http_client,
+            connection_conf: connection_config,
+            domain,
+        },
+    };
+    // let count = mailbox.count(None).await;
+    // info!("Count:{:?}", count);
+    //
+    // let default_ism = mailbox.default_ism().await;
+    // info!("Default ism:{:?}", default_ism);
+
+    let delivered = mailbox.delivered(H256::from_low_u64_be(1)).await;
+    info!("Delivered:{:?}", delivered);
 
     Ok(())
 }
