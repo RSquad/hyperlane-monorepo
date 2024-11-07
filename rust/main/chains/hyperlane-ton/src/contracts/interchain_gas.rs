1ae067033d349fa98d4a48c33c20ceb89ed1dabb
Lapo4kaKek <Sheydos2003@yandex.ru>
2024-11-07 11:35:59 +0000

    refactor(get_finalized_block_number): switch to workchain -1

--- rust/main/chains/hyperlane-ton/src/contracts/interchain_gas.rs
+++ rust/main/chains/hyperlane-ton/src/contracts/interchain_gas.rs
@@ -182,7 +182,7 @@ impl Indexer<InterchainGasPayment> for TonInterchainGasPaymaster {
         let response = self
             .provider
             .get_blocks(
-                self.workchain,
+                -1,
                 None,
                 None,
                 None,
