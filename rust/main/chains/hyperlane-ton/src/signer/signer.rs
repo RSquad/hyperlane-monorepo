f69ea8c0bbf07b097e3078d47b094313d24a714a
Lapo4kaKek <Sheydos2003@yandex.ru>
2024-11-16 10:23:37 +0000

    feature:(TonSigner): added create_signed_message and constructor from_mnemonic in TonSigner

--- rust/main/chains/hyperlane-ton/src/signer/signer.rs
+++ rust/main/chains/hyperlane-ton/src/signer/signer.rs
@@ -1,9 +1,9 @@
 use anyhow::Error;
-use hyperlane_core::ChainCommunicationError::HyperlaneSignerError;
+use hyperlane_core::{ChainCommunicationError, ChainCommunicationError::HyperlaneSignerError};
 use log::warn;
 use tonlib_core::{
-    cell::Cell,
-    mnemonic::KeyPair,
+    cell::{ArcCell, BagOfCells, Cell},
+    mnemonic::{KeyPair, Mnemonic},
     wallet::{TonWallet, WalletVersion},
     TonAddress,
 };
@@ -12,7 +12,7 @@ use tonlib_core::{
 pub struct TonSigner {
     pub address: TonAddress,
     private_key: Vec<u8>,
-    wallet: TonWallet,
+    pub wallet: TonWallet,
 }
 
 impl TonSigner {
@@ -26,6 +26,21 @@ impl TonSigner {
             wallet,
         })
     }
+    pub fn from_mnemonic(
+        mnemonic_phrase: Vec<String>,
+        wallet_version: WalletVersion,
+    ) -> Result<Self, Error> {
+        let mnemonic_phrase_str: Vec<&str> =
+            mnemonic_phrase.iter().map(|item| item.as_str()).collect();
+        let mnemonic = Mnemonic::new(mnemonic_phrase_str, &None)
+            .expect("Failed to create Mnemonic from phrase")?;
+
+        let key_pair = mnemonic
+            .to_key_pair()
+            .expect("Failed to generate KeyPair from mnemonic")?;
+
+        Self::new(key_pair, wallet_version)
+    }
     pub async fn sign_message(&self, body: &Cell) -> Result<Vec<u8>, Error> {
         let signature = self
             .wallet
@@ -33,5 +48,26 @@ impl TonSigner {
             .map_err(|e| Error::new(e))?;
 
         Ok(signature.data().to_vec())
+    }
+    pub async fn create_signed_message(
+        &self,
+        transfer_message: Cell,
+        now: u32,
+        seqno: u32,
+    ) -> Result<String, ChainCommunicationError> {
+        let message = self
+            .wallet
+            .create_external_message(now + 60, seqno, vec![ArcCell::new(transfer_message)], false)
+            .map_err(|e| {
+                ChainCommunicationError::CustomError(format!("Failed to create message: {}", e))
+            })?;
+
+        let boc = BagOfCells::from_root(message)
+            .serialize(true)
+            .map_err(|e| {
+                ChainCommunicationError::CustomError(format!("Failed to serialize BOC: {}", e))
+            })?;
+
+        Ok(base64::encode(boc))
     }
 }
