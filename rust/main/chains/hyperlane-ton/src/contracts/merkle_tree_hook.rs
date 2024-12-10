0ae1893e7b397dfea094da9dbd9ed93206eea66d
Lapo4kaKek <Sheydos2003@yandex.ru>
2024-12-10 08:06:17 +0300

    feat(merkle-tree-hok): init implementation merkle_tree_hook

--- rust/main/chains/hyperlane-ton/src/contracts/merkle_tree_hook.rs
+++ rust/main/chains/hyperlane-ton/src/contracts/merkle_tree_hook.rs
@@ -1,10 +1,13 @@
 use crate::client::provider::TonProvider;
+use crate::ton_api_center::TonApiCenter;
 use crate::utils::conversion::ConversionUtils;
 use async_trait::async_trait;
 use hyperlane_core::accumulator::incremental::IncrementalMerkle;
+use hyperlane_core::accumulator::{TREE_DEPTH, ZERO_HASHES};
 use hyperlane_core::{
-    ChainResult, Checkpoint, HyperlaneChain, HyperlaneContract, HyperlaneDomain, HyperlaneProvider,
-    Indexed, Indexer, LogMeta, MerkleTreeHook, MerkleTreeInsertion, SequenceAwareIndexer, H256,
+    ChainCommunicationError, ChainResult, Checkpoint, HyperlaneChain, HyperlaneContract,
+    HyperlaneDomain, HyperlaneProvider, Indexed, Indexer, LogMeta, MerkleTreeHook,
+    MerkleTreeInsertion, SequenceAwareIndexer, H256,
 };
 use std::ops::RangeInclusive;
 use tonlib_core::TonAddress;
@@ -44,18 +47,97 @@ impl HyperlaneChain for TonMerkleTreeHook {
 #[async_trait]
 impl MerkleTreeHook for TonMerkleTreeHook {
     async fn tree(&self, _lag: Option<std::num::NonZeroU64>) -> ChainResult<IncrementalMerkle> {
-        Ok(IncrementalMerkle::default())
+        let count = self.count(_lag).await?;
+        let mut branch: [H256; TREE_DEPTH] = Default::default();
+        branch
+            .iter_mut()
+            .enumerate()
+            .for_each(|(i, elem)| *elem = ZERO_HASHES[i]);
+        Ok(IncrementalMerkle {
+            branch,
+            count: count as usize,
+        })
     }
 
     async fn count(&self, _lag: Option<std::num::NonZeroU64>) -> ChainResult<u32> {
-        Ok(1)
+        let response = self
+            .provider
+            .run_get_method(self.address.to_string(), "get_count".to_string(), None)
+            .await;
+
+        match response {
+            Ok(run_get_method) => {
+                if let Some(stack_item) = run_get_method.stack.get(0) {
+                    if let Ok(count) = u32::from_str_radix(&stack_item.value[2..], 16) {
+                        Ok(count)
+                    } else {
+                        Err(ChainCommunicationError::CustomError(
+                            "Failed to parse count".to_string(),
+                        ))
+                    }
+                } else {
+                    Err(ChainCommunicationError::CustomError(
+                        "Empty stack in response".to_string(),
+                    ))
+                }
+            }
+            Err(e) => Err(ChainCommunicationError::CustomError(
+                "Failed to get response".to_string(),
+            )),
+        }
     }
 
     async fn latest_checkpoint(
         &self,
         _lag: Option<std::num::NonZeroU64>,
     ) -> ChainResult<Checkpoint> {
-        todo!()
+        let response = self
+            .provider
+            .run_get_method(
+                self.address.to_string(),
+                "get_latest_checkpoint".to_string(),
+                None,
+            )
+            .await;
+
+        match response {
+            Ok(run_get_method) => {
+                let stack = run_get_method.stack;
+
+                if stack.len() < 2 {
+                    return Err(ChainCommunicationError::CustomError(
+                        "Stack does not contain enough elements".to_string(),
+                    ));
+                }
+
+                let root_item = &stack[0];
+                let root_value = &root_item.value;
+
+                let root = u32::from_str_radix(&root_value[2..], 16).map_err(|_| {
+                    ChainCommunicationError::CustomError("Failed to parse root value".to_string())
+                })?;
+
+                let index_item = &stack[1];
+                let index_value = &index_item.value;
+                let index = u32::from_str_radix(&index_value[2..], 16).map_err(|_| {
+                    ChainCommunicationError::CustomError("Failed to parse index value".to_string())
+                })?;
+
+                Ok(Checkpoint {
+                    merkle_tree_hook_address: ConversionUtils::ton_address_to_h256(
+                        &self.address.clone(),
+                    )
+                    .unwrap(),
+                    mailbox_domain: 777001,
+                    root: H256::from_low_u64_be(root as u64),
+                    index,
+                })
+            }
+            Err(e) => Err(ChainCommunicationError::CustomError(format!(
+                "Failed to get response: {:?}",
+                e
+            ))),
+        }
     }
 }
 
