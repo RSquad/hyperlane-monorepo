fbbc27719406c51c5a01b156038ebaebb82dd1e8
Lapo4kaKek <Sheydos2003@yandex.ru>
2024-12-31 03:10:09 +0300

    reafctor(error): added new error type in enum

--- rust/main/chains/hyperlane-ton/src/error.rs
+++ rust/main/chains/hyperlane-ton/src/error.rs
@@ -22,6 +22,10 @@ pub enum HyperlaneTonError {
     ApiRequestFailed(String),
     #[error("Conversion data failed")]
     ConversionFailed(String),
+    #[error("Failed to parse stack item: {0}")]
+    FailedToParseStackItem(String),
+    #[error("Failed to build cell: {0}")]
+    FailedBuildingCell(String),
     #[error("Reqwest error: {0}")]
     ReqwestError(#[from] reqwest::Error),
     /// Error while making a call to a smart contract
@@ -38,6 +42,8 @@ pub enum HyperlaneTonError {
     ParsingError(String),
     #[error("Failed to construct URL: {0}")]
     UrlConstructionError(String),
+    #[error("Unknown module type value: {0}")]
+    UnknownModuleType(u32),
     #[error("No transaction found for the provided hash")]
     TransactionNotFound,
     /// Invalid configuration
