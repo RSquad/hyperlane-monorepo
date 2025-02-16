23f011d1971a2d05930092143839ae202d971a50
NikitaM <21960067+Keshoid@users.noreply.github.com>
2025-02-16 10:51:23 +0300

    chore: upd deploy script

--- altvm_contracts/ton/scripts/deployTokenCollateral.ts
+++ altvm_contracts/ton/scripts/deployTokenCollateral.ts
@@ -1,14 +1,19 @@
 import { NetworkProvider, compile } from '@ton/blueprint';
-import { toNano } from '@ton/core';
+import { Address, toNano } from '@ton/core';
 
 import { TokenCollateral } from '../wrappers/TokenCollateral';
 
 export async function run(provider: NetworkProvider) {
   const tokenCollateral = provider.open(
-    TokenCollateral.createFromConfig({}, await compile('TokenCollateral')),
+    TokenCollateral.createFromConfig(
+      {
+        jettonAddress: Address.parse(''),
+        mailboxAddress: Address.parse(''),
+      },
+      await compile('TokenCollateral'),
+    ),
   );
 
-  await tokenCollateral.sendDeploy(provider.sender(), toNano('0.05'));
-
+  await tokenCollateral.sendDeploy(provider.sender(), toNano('0.1'));
   await provider.waitForDeploy(tokenCollateral.address);
 }
