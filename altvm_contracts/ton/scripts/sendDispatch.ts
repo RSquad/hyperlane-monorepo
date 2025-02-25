165c8164b6da50eab46d3e182de9a8b009d8da76
Vasilii Lazarev <97127102+Lapo4kaKek@users.noreply.github.com>
2025-02-25 15:25:50 +0300

    fix: rename 'message' to 'messageBody' and convert hookMetadata to cell (#47)

--- altvm_contracts/ton/scripts/sendDispatch.ts
+++ altvm_contracts/ton/scripts/sendDispatch.ts
@@ -43,7 +43,7 @@ export async function run(provider: NetworkProvider) {
   await mailbox.sendDispatch(provider.sender(), toNano('0.5'), {
     destDomain: targetDomain,
     recipientAddr: destAddrTon,
-    message: beginCell().storeStringTail('Hello, world!').endCell(),
-    hookMetadata,
+    messageBody: beginCell().storeStringTail('Hello, world!').endCell(),
+    hookMetadata: hookMetadata.toCell(),
   });
 }
