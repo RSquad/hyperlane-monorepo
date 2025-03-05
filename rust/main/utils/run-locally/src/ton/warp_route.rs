02c606b0cc065a470678d6f6443810c8b56eab47
Lapo4kaKek <Sheydos2003@yandex.ru>
2025-03-05 12:47:01 +0300

    feat: added script call for burn jetton, balance check, and raw bridge backtracking

--- rust/main/utils/run-locally/src/ton/warp_route.rs
+++ rust/main/utils/run-locally/src/ton/warp_route.rs
@@ -1,7 +1,11 @@
-use log::info;
+use hyperlane_ton::ton_api_center::TonApiCenter;
+use hyperlane_ton::TonProvider;
+use log::{info, warn};
+use std::error::Error;
 //use macro_rules_attribute::apply;
 use std::{env, fs, thread::sleep, time::Duration};
 //use tempfile::tempdir;
+use serde_json::Value;
 
 use crate::ton::launch_ton_relayer;
 use crate::ton::launch_ton_scraper;
@@ -15,53 +19,75 @@ use crate::{
     ton::utils::build_rust_bins,
     utils::{as_task, concat_path, make_static, stop_child, AgentHandles, TaskHandle},
 };
+use hyperlane_core::HyperlaneDomain;
+use hyperlane_core::KnownHyperlaneDomain;
+use hyperlane_ton::TonConnectionConf;
+use reqwest::Client;
+use url::Url;
 
 use crate::ton::TonHyperlaneStack;
 
 #[allow(dead_code)]
-pub fn run_ton_to_ton_warp_route() {
+pub async fn run_ton_to_ton_warp_route() {
     info!("Start run_locally() for Ton");
     let domains: Vec<u32> = env::var("DOMAINS")
         .expect("DOMAINS env variable is missing")
         .split(',')
         .map(|d| d.parse::<u32>().expect("Invalid domain format"))
         .collect();
-    let origin_token_standard =
-        env::var("ORIGIN_TOKEN_STANDARD").expect("Failed to get ORIGIN_TOKEN_STANDARD");
-    let destination_token_standart =
-        env::var("DESTINATION_TOKEN_STANDARD").expect("Failed to get DESTINATION_TOKEN_STANDARD");
-    let validator_key = "0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a";
+    // let origin_token_standard =
+    //     env::var("ORIGIN_TOKEN_STANDARD").expect("Failed to get ORIGIN_TOKEN_STANDARD");
+    // let destination_token_standart =
+    //     env::var("DESTINATION_TOKEN_STANDARD").expect("Failed to get DESTINATION_TOKEN_STANDARD");
+    // let validator_key = "0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a";
 
     info!("domains:{:?}", domains);
 
-    deploy_and_setup_domains(&domains, &validator_key);
+    // deploy_and_setup_domains(&domains, &validator_key);
 
-    for &domain in &domains {
-        let domain_str = &format!("{}", domain);
-        let deployed_contracts_addresses = read_deployed_contracts(domain_str);
-        let mailbox_address = deployed_contracts_addresses
-            .get("mailboxAddress")
-            .expect("Not found mailbox");
-        deploy_warp_route(
-            domain,
-            origin_token_standard.as_str(),
-            destination_token_standart.as_str(),
-            mailbox_address.as_str(),
-        )
-        .expect("Failed to deploy warp route");
-    }
-    let amount = env::var("AMOUNT")
-        .expect("Failed to get amount")
-        .parse::<u64>()
-        .expect("Failed");
+    // for &domain in &domains {
+    //     let domain_str = &format!("{}", domain);
+    //     let deployed_contracts_addresses = read_deployed_contracts(domain_str);
+    //     let mailbox_address = deployed_contracts_addresses
+    //         .get("mailboxAddress")
+    //         .expect("Not found mailbox");
+    //     deploy_warp_route(
+    //         domain,
+    //         origin_token_standard.as_str(),
+    //         destination_token_standart.as_str(),
+    //         mailbox_address.as_str(),
+    //     )
+    //     .expect("Failed to deploy warp route");
+    // }
+    // let amount = env::var("AMOUNT")
+    //     .expect("Failed to get amount")
+    //     .parse::<u64>()
+    //     .expect("Failed");
 
-    let recipient = "";
-    send_transfer(domains[0], domains[1], amount, recipient).expect("Failed to send transfer");
-
+    // let recipient = "";
+    // send_transfer(domains[0], domains[1], amount, recipient).expect("Failed to send transfer");
     info!("deploy_all_contracts and send_dispatch finished!");
+
     let mnemonic = env::var("MNEMONIC").expect("MNEMONIC env is missing");
     let wallet_version = env::var("WALLET_VERSION").expect("WALLET_VERSION env is missing");
     let api_key = env::var("API_KEY").expect("API_KEY env is missing");
+
+    let http_client = Client::new();
+    let api_url = "https://testnet.toncenter.com/api/";
+    let connection_conf = TonConnectionConf::new(
+        Url::parse(api_url).expect("Failed to parse url"),
+        api_key.to_string(),
+        5,
+    );
+    let domain = HyperlaneDomain::Known(KnownHyperlaneDomain::TonTest1); // It doesn't matter now.
+
+    let provider = TonProvider::new(http_client, connection_conf, domain);
+
+    let initial_balance: u128 = get_balance(&provider, domains[0])
+        .await
+        .expect("Failed to get initial_balance");
+
+    info!("Initial jetton wallet balance: {}", initial_balance);
 
     log!("Building rust...");
     build_rust_bins(&["relayer", "validator", "scraper", "init-db"]);
@@ -129,20 +155,34 @@ pub fn run_ton_to_ton_warp_route() {
 
     let validators = vec![validator1, validator2];
 
-    let scraper = launch_ton_scraper(
-        agent_config_path.clone(),
-        relay_chains.clone(),
-        scraper_metrics_port,
-        debug,
-    );
+    // let scraper = launch_ton_scraper(
+    //     agent_config_path.clone(),
+    //     relay_chains.clone(),
+    //     scraper_metrics_port,
+    //     debug,
+    // );
 
-    info!("Waiting for agents to run for 3 minutes...");
-    sleep(Duration::from_secs(300));
+    info!("Waiting for agents to run for 1 minutes...");
+    sleep(Duration::from_secs(60));
+    let current_balance: u128 = get_balance(&provider, domains[1])
+        .await
+        .expect("Failed to get initial_balance");
+    if (current_balance <= initial_balance) {
+        warn!("current_balance <= initial_balance");
+    }
+    send_burn(domains[1], domains[0], 1, "");
+    info!("Send burn executed, waiting for agents to run for 1 minutes...");
+    let balance_after_burn = get_balance(&provider, domains[1])
+        .await
+        .expect("Failed to get initial_balance");
+    if (balance_after_burn >= current_balance) {
+        warn!("burn failed because balance_after_burn >= current_balance");
+    }
+    sleep(Duration::from_secs(60));
 
     let _ = TonHyperlaneStack {
         validators: validators.into_iter().map(|v| v.join()).collect(),
         relayer: relayer.join(),
-        scraper: scraper.join(),
         postgres,
     };
 }
@@ -233,4 +273,103 @@ pub fn send_transfer(
     log!("stdout:\n{}", stdout);
 
     Ok(())
+}
+
+pub fn send_burn(
+    origin_domain: u32,
+    dest_domain: u32,
+    amount: u64,
+    recipient: &str,
+) -> Result<(), String> {
+    log!("Launching sendTransfer script...");
+
+    let working_dir = "../../../../altvm_contracts/ton";
+
+    let output = Command::new("yarn")
+        .arg("run")
+        .arg("warp:burn")
+        .env("ORIGIN_DOMAIN", origin_domain.to_string())
+        .env("DESTINATION_DOMAIN", dest_domain.to_string())
+        .env("AMOUNT", amount.to_string())
+        .env("RECIPIENT", recipient)
+        .current_dir(working_dir)
+        .output()
+        .expect("Failed to execute sendTransfer");
+
+    let stdout = from_utf8(&output.stdout).unwrap_or("[Invalid UTF-8]");
+    let stderr = from_utf8(&output.stderr).unwrap_or("[Invalid UTF-8]");
+
+    if !output.status.success() {
+        log!("warp burn failed with status: {}", output.status);
+        log!("stderr:\n{}", stderr);
+        return Err(format!(
+            "send burn failed with status: {}\nstderr:\n{}",
+            output.status, stderr
+        ));
+    }
+
+    log!("warp burn script executed successfully!");
+    log!("stdout:\n{}", stdout);
+
+    Ok(())
+}
+
+pub async fn get_balance(
+    provider: &TonProvider,
+    domain: u32,
+) -> Result<u128, Box<dyn std::error::Error>> {
+    let warp_json = read_warp_contracts(domain).expect("Failed to read warp contracts");
+
+    let jetton_wallet_address = warp_json
+        .get("jetton")
+        .and_then(|v| v.as_str())
+        .expect("Jetton wallet address not found in JSON");
+
+    let initial_response = provider
+        .get_jetton_wallets(
+            None,
+            Some(vec!["".to_string()]),
+            Some(vec![jetton_wallet_address.to_string()]),
+            None,
+            Some(100),
+            Some(0),
+            None,
+        )
+        .await
+        .expect("Failed to jettons");
+
+    let first_wallet = initial_response
+        .jetton_wallets
+        .get(0)
+        .expect("No wallets found in the response");
+
+    let balance: u128 = first_wallet
+        .balance
+        .parse()
+        .expect("Invalid balance format");
+    return Ok(balance);
+}
+
+pub fn read_warp_contracts(domain: u32) -> Option<Value> {
+    let file_path = format!(
+        "../../../../altvm_contracts/ton/warp-contracts-{}.json",
+        domain
+    );
+
+    match fs::read_to_string(&file_path) {
+        Ok(content) => match serde_json::from_str::<Value>(&content) {
+            Ok(json) => {
+                log!("Successfully read warp contracts from: {}", file_path);
+                Some(json)
+            }
+            Err(err) => {
+                log!("Failed to parse JSON from {}: {}", file_path, err);
+                None
+            }
+        },
+        Err(err) => {
+            log!("Failed to read file {}: {}", file_path, err);
+            None
+        }
+    }
 }
